a8e76036cf77a195a25e5f458885bdea863c593a
aclswarm <aerospacecontrolslabmit@gmail.com>
2022-10-21 14:52:00 -0400

    minor

--- rmader/scripts/position_exchange.py
+++ rmader/scripts/position_exchange.py
@@ -229,7 +229,8 @@ class TermGoalSender:
                     self.term_goal.pose.position.x = self.wp8[0]
                     self.term_goal.pose.position.y = self.wp8[1]
 
-            self.term_goal.pose.position.z = 1.0 + 2.0 * random()
+            # self.term_goal.pose.position.z = 1.0 + 2.0 * random()
+            self.term_goal.pose.position.z = 1.5
             # self.term_goal.pose.position.z = 1.0
 
             self.if_arrived = not self.if_arrived
