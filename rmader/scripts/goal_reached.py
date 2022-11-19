7e3a0ff4c84313f6b2d41981b9ed9508e87da3ec
kotakondo <kkondo@mit.edu>
2022-11-19 16:48:58 -0500

    minor

--- rmader/scripts/goal_reached.py
+++ rmader/scripts/goal_reached.py
@@ -25,7 +25,7 @@ class GoalReachedCheck:
         rospy.sleep(3)
 
         # goal radius
-        self.goal_radius = 0.15
+        self.goal_radius = 0.30
 
         # number of agents
         self.num_of_agents = 16
