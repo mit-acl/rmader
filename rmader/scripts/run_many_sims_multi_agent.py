b64a55f6f1feabbfbee15257417e1bb66019d61f
kotakondo <kkondo@mit.edu>
2022-11-20 13:45:50 -0500

    param go back to the successful ones on google cloud

--- rmader/scripts/run_many_sims_multi_agent.py
+++ rmader/scripts/run_many_sims_multi_agent.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     num_of_sims=5
     num_of_agents=10
     radius=10
-    how_long_to_wait=60 #[s]
+    how_long_to_wait=30 #[s]
     cd_list = [0, 50, 100, 200, 300]
 
     ##### loop
