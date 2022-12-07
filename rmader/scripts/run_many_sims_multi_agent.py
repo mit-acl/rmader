f842ea8b2ccefc6b3875f5da25bfeade721edadf
kotakondo <kkondo@mit.edu>
2022-12-07 17:31:23 -0500

    small bbox for obs sims

--- rmader/scripts/run_many_sims_multi_agent.py
+++ rmader/scripts/run_many_sims_multi_agent.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     num_of_agents=10
     radius=10
     how_long_to_wait=60 #[s]
-    cd_list = [0, 50, 100]
+    cd_list = [0, 50, 100, 200, 300]
 
     ##### loop
     for cd in cd_list:
