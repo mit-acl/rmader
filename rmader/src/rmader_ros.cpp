5f2528baef5eb968aa9ee57ee91fa5b0373bdb2d
kotakondo <kkondo@mit.edu>
2022-12-07 17:50:03 -0500

    using nh1 instead of nh4

--- rmader/src/rmader_ros.cpp
+++ rmader/src/rmader_ros.cpp
@@ -231,7 +231,7 @@ RmaderRos::RmaderRos(ros::NodeHandle nh1, ros::NodeHandle nh2, ros::NodeHandle n
       std::cout << agent << std::endl;
       if (myns != agent)
       {  // if my namespace is the same as the agent, then it's you
-        sub_traj_.push_back(nh4_.subscribe(agent + "/rmader/trajs", 10, &RmaderRos::trajCB,
+        sub_traj_.push_back(nh1_.subscribe(agent + "/rmader/trajs", 10, &RmaderRos::trajCB,
                                            this));  // The number is the queue size
       }
     }
@@ -248,7 +248,7 @@ RmaderRos::RmaderRos(ros::NodeHandle nh1, ros::NodeHandle nh2, ros::NodeHandle n
       (id <= 9) ? agent = "/" + veh + "0" + std::to_string(id) + "s" : agent = "/" + veh + std::to_string(id) + "s";
       if (myns != agent)
       {  // if my namespace is the same as the agent, then it's you
-        sub_traj_.push_back(nh4_.subscribe(agent + "/rmader/trajs", 10, &RmaderRos::trajCB,
+        sub_traj_.push_back(nh1_.subscribe(agent + "/rmader/trajs", 10, &RmaderRos::trajCB,
                                            this));  // The number is the queue size
       }
     }
