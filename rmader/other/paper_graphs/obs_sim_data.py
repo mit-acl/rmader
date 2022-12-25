28643b14e7aa4448ac4e5f5ee0af8ad89c015e2b
kotakondo <kkondo@mit.edu>
2022-12-25 14:57:26 -0500

    obstacle agent collision check using tf after sims

--- rmader/other/paper_graphs/obs_sim_data.py
+++ rmader/other/paper_graphs/obs_sim_data.py
@@ -20,6 +20,8 @@ import statistics
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as font_manager
 import sys
+from tf_bag import BagTfTransformer
+
 
 if __name__ == '__main__':
 
@@ -27,19 +29,18 @@ if __name__ == '__main__':
     home_dir = "/media/kota/T7/rmader_ral/mader/rmader_obs"
     num_of_agents = 10
     source_dir = home_dir+"/bags/cd50ms/dc125ms"           
-    source_dir_len = len(source_dir)
+    source_len = len(source_dir)
     source_dir = home_dir+"/bags/cd50ms/dc125ms/*.bag"           
     rosbag_list = glob.glob(source_dir)
     rosbag_list.sort() #alphabetically order
     rosbags = []
+    bbox_limit = 0.05
 
     for bag in rosbag_list:
         rosbags.append(bag)
     
-    # collision when not stopped
-    collision_cnt_not_stopped = 0
-    # collision when stopped
-    collision_cnt_stopped = 0
+    # collision count
+    collision_cnt = 0
     # total dist
     total_dist = 0.0
     # travel time
@@ -47,25 +48,63 @@ if __name__ == '__main__':
     # stop count
     stop_cnt = 0.0
 
-    for bag in rosbags:
-        print('rosbag ' + str(bag))
-        tf_buffer = tf2_py.BufferCore(rospy.Duration(1000))
-        print('hi')
-        bag = rosbag.Bag(bag)
-        for topic, msg, t in bag.read_messages(topics=['/tf']):
-            print(topic)
-            print(msg)
-            
-            for msg_tf in msg.transforms:
-                tf_buffer.set_transform("SQ01s", "SQ02s")
-            print(tf_buffer)
+    agent_obs_list = []
+    for i in range(1,11):
+        if i<=9:
+            agent = "SQ0" + str(i) + "s" 
+        else:
+            agent = "SQ" + str(i) + "s"
+        agent_obs_list.append(agent)
+    for j in range(4000, 4010):
+        agent_obs_list.append(f"obs{j}")
 
-    ave_dist = total_dist / num_of_agents
+    for rosbag_name in rosbags:
+        print('rosbag ' + str(rosbag_name))
+        sim_id = rosbag_name[source_len+5:source_len+8]
+        bag = rosbag.Bag(rosbag_name)
+        bag_transformer = BagTfTransformer(bag)
+        
+        b = bagreader(rosbag_name, verbose=False)
+        log_data = b.message_by_topic("/SQ01s/goal")
+        log = pd.read_csv(log_data, usecols=["header.stamp.secs", "header.stamp.nsecs"])
+        log.columns = ["secs", "nsecs"]
+        t_go_bag = log.secs[0] + log.nsecs[0] / 1e+9
+        final_time_bag=bag.get_end_time()
+        total_time_bag=final_time_bag-t_go_bag;
+        times_discret = np.linspace(t_go_bag, final_time_bag, 2000)
 
-    os.system(f'echo "{test}" >> '+home_dir+'/ave_dist.txt')
-    os.system('echo "ave dist '+str(round(ave_dist,3))+'m" >> '+home_dir+'/ave_dist.txt')
-    os.system('echo "------------------------------------------------------------" >> '+home_dir+'/ave_dist.txt')
+        for agent1 in agent_obs_list:
+            for agent2 in agent_obs_list: 
+                if agent1 == agent2:
+                    break
 
-    os.system(f'echo "{test}" >> '+home_dir+'/max_vel.txt')
-    os.system('echo "max vel '+str(max_vel)+'" >> '+home_dir+'/max_vel.txt')
-    os.system('echo "------------------------------------------------------------" >> '+home_dir+'/max_vel.txt')
+                for ii in range(len(times_discret)-1):
+                    time=times_discret[ii];
+                    
+                    if agent1[:2] == "SQ":
+                        parent_frame1 = "vicon"
+                    else:
+                        parent_frame1 = "world"
+
+                    if agent2[:2] == "SQ":
+                        parent_frame2 = "vicon"
+                    else:
+                        parent_frame2 = "world"
+
+                    translation1, quaternion = bag_transformer.lookupTransform(parent_frame1, agent1, rospy.Time.from_sec(time))
+                    translation2, quaternion = bag_transformer.lookupTransform(parent_frame2, agent2, rospy.Time.from_sec(time))
+                    dist=np.linalg.norm(np.array(translation1)-np.array(translation2), ord=np.inf)
+                    print(dist)
+
+                    if (dist < 0.05):
+                        collision_cnt += 1
+                        print("sim " + sim_id + ": ******collision******" )
+                        os.system(f'echo "simulation '+sim_id+': ***collision*** in {time} with {agent1} and {agent2}" >> '+source_dir+'/collision_status.txt')
+
+        print("sim " + sim_id + ": no collision" )
+        os.system('echo "simulation '+sim_id+': no collision" >> '+source_dir+'/collision_status.txt')    
+    
+    collision_per = collision_cnt
+    os.system('echo "'+source_dir+'" >> '+home_dir+'/collision_count.txt')
+    os.system('echo "'+str(collision_cnt)+'/'+str(len(rosbag))+' - '+str(round(collision_per,2))+'%" >> '+home_dir+'/collision_count.txt')
+    os.system('echo "------------------------------------------------------------" >> '+home_dir+'/collision_count.txt')
