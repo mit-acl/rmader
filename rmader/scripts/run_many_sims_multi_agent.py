8c484d0ff149620f986bb64566629a80862f65ef
kotakondo <kkondo@mit.edu>
2023-01-24 17:38:04 -0500

    minor

--- rmader/scripts/run_many_sims_multi_agent.py
+++ rmader/scripts/run_many_sims_multi_agent.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     num_of_agents=10
     radius=10
     how_long_to_wait=40 #[s]
-    cd_list = [0, 50, 100, 200, 300]
+    cd_list = [50]
 
     ##### loop
     for cd in cd_list:
@@ -76,9 +76,9 @@ if __name__ == '__main__':
                     folder_txts=source_dir + "/oldmader/txt_files/cd"+str(cd)+"ms"
                     folder_csv=source_dir + "/oldmader/csv/cd"+str(cd)+"ms"
                 else:
-                    folder_bags=source_dir +"/rmader/bags/cd"+str(cd)+"ms/dc"+str_dc+"ms"
-                    folder_txts=source_dir +"/rmader/txt_files/cd"+str(cd)+"ms/dc"+str_dc+"ms"
-                    folder_csv=source_dir +"/rmader/csv/cd"+str(cd)+"ms/dc"+str_dc+"ms"
+                    folder_bags=source_dir +"/rmader_obs/bags/cd"+str(cd)+"ms/dc"+str_dc+"ms"
+                    folder_txts=source_dir +"/rmader_obs/txt_files/cd"+str(cd)+"ms/dc"+str_dc+"ms"
+                    folder_csv=source_dir +"/rmader_obs/csv/cd"+str(cd)+"ms/dc"+str_dc+"ms"
             else: ## on my desktop
                 source_dir = "/home/kota/test/data"
                 if is_oldmader:
