62e59bdb2ebae2ef76cc35a2c054804a9c1a21cc
aclswarm <aerospacecontrolslabmit@gmail.com>
2023-02-03 18:48:57 -0500

    Testing 2/3/23.

--- rmader/scripts/obstacles/generate_traj_params.py
+++ rmader/scripts/obstacles/generate_traj_params.py
@@ -25,8 +25,13 @@ zmin=1.0
 zmax=2.5
 
 all_drones=[     #"name",     "bbox",      "slower", "offset", "lim_x",    "lim_y",    "lim_z"
-            Drone("SQ02s", [0.7, 0.7, 0.7],  3.5,   0.0,   [9.0, 13.0], [-2.0, 2.0],   [zmin,zmax])
+            Drone("obstacle2", [0.8
+                , 0.8
+            , 0.8
+            ],  5.5,   0.0,   [9.0, 13.0], [-2.0, 2.0],   [zmin,zmax])
             ]
+
+all_drones.append(Drone("obstacle1", [0.8, 0.8, 0.8],  5.5,   0.0,   [-2.0, 2.0], [-2.0, 2.0],   [zmin,zmax]))
 
 tmp = rospkg.RosPack()
 pwd_package=tmp.get_path('rmader')
@@ -35,7 +40,7 @@ for i in range(len(all_drones)):
     drone_i=all_drones[i];
     traj=getTrefoil(t, drone_i.offset, drone_i.slower, drone_i.lim_x, drone_i.lim_y, drone_i.lim_z)
     # print traj
-    name_file=pwd_package+"/param/obstacle2.yaml"
+    name_file=pwd_package+f"/param/{drone_i.name}.yaml"
     f = open(name_file, "w")
     f.write("# DO NOT EDIT. RUN THE PYTHON FILE INSTEAD TO GENERATE THIS .yaml FILE \n")
     f.write("traj_x: "+str(traj[0])+"\n")
