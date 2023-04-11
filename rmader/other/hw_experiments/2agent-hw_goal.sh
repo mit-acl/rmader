b466e4ac646448378a5e65e4c6b51d7e6b6d25cd
aclswarm <aerospacecontrolslabmit@gmail.com>
2023-04-11 17:41:47 -0400

    minor

--- rmader/other/hw_experiments/2agent-hw_goal.sh
+++ rmader/other/hw_experiments/2agent-hw_goal.sh
@@ -35,15 +35,19 @@ done
 sleep 1
 
 # ssh each nuc
-tmux send-keys -t $SESSION:$w.0 "ssh nuc1@192.168.15.2" C-m
-tmux send-keys -t $SESSION:$w.1 "ssh nuc9@192.168.23.2" C-m
+# tmux send-keys -t $SESSION:$w.0 "ssh nuc1@192.168.15.2" C-m
+# tmux send-keys -t $SESSION:$w.1 "ssh nuc9@192.168.23.2" C-m
+tmux send-keys -t $SESSION:$w.0 "ssh nuc2@192.168.16.2" C-m
+tmux send-keys -t $SESSION:$w.1 "ssh nuc3@192.168.17.2" C-m
 
 sleep 1
 
 # send goals (randomly generated or position exchange)
 if [ "$1" == "pos" ]; then
-	tmux send-keys -t $SESSION:$w.0 "roslaunch rmader position_exchange.launch mode:=1 quad:=NX01"  
-	tmux send-keys -t $SESSION:$w.1 "roslaunch rmader position_exchange.launch mode:=2 quad:=NX09"
+	tmux send-keys -t $SESSION:$w.0 "roslaunch rmader position_exchange.launch mode:=1 quad:=NX02"  
+	tmux send-keys -t $SESSION:$w.1 "roslaunch rmader position_exchange.launch mode:=2 quad:=NX03"
+	# tmux send-keys -t $SESSION:$w.1 "roslaunch rmader position_exchange.launch mode:=2 quad:=NX09"
+	# tmux send-keys -t $SESSION:$w.0 "roslaunch rmader position_exchange.launch mode:=1 quad:=NX01"  
 fi
 
 tmux -2 attach-session -t $SESSION
